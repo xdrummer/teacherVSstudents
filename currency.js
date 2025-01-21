2c5e713b67588013c9daf18c5298b7d3d02f3270
Neruux <75936969+Neruux@users.noreply.github.com>
2025-01-21 07:55:02 +0100

    da

--- currency.js
+++ currency.js
@@ -1,26 +1,83 @@
 var flipchart = document.getElementById("Canvas");
 var papier = flipchart.getContext("2d");
 
-var currencys = [];
+var currencies = [];
+var drawID;
+var updateID;
+var createID;
+var score = 0; // Variable zur Speicherung des Punktestands
 
-class Currency{
-    constructor(){
-        this.x = Math.random()*flipchart.height;
+class Currency {
+    constructor() {
+        this.x = Math.random() * flipchart.width; // Zufällige X-Position
         this.y = 0;
         this.bild = new Image();
-        this.bild.src = "Bilder/ufo.png";
+        this.bild.src = "sun_test.png";
+        this.scale = 0.2;
 
-
- 
+        this.bild.onload = () => {
+            this.width = this.bild.width * this.scale;
+            this.height = this.bild.height * this.scale;
+        };
     }
 
-    fall = function(){
-        this.y = this.y + 1;
-    }
+    fall = function () {
+        if (this.y < flipchart.height - this.height) {
+            this.y = this.y + 1;
+        }
+    };
 
+    contains = function (mouseX, mouseY) {
+        return (
+            mouseX >= this.x &&
+            mouseX <= this.x + this.width &&
+            mouseY >= this.y &&
+            mouseY <= this.y + this.height
+        );
+    };
 }
 
+function onload() {
+    updateID = window.setInterval(() => {
+        update();
+    }, 10);
 
-for(let i=0;i<currencys.length;i=i+1){
-    papier.drawImage(currencys[i].bild, currencys[i].x, currencys[i].y);
+    createID = window.setInterval(() => {
+        currencies.push(new Currency());
+    }, 3000);
+
+    drawID = draw();
+
+    flipchart.addEventListener("click", (e) => {
+        const rect = flipchart.getBoundingClientRect();
+        const mouseX = e.clientX - rect.left;
+        const mouseY = e.clientY - rect.top;
+
+        // Prüfen, ob ein Currency-Objekt angeklickt wurde
+        for (let i = currencies.length - 1; i >= 0; i--) {
+            if (currencies[i].contains(mouseX, mouseY)) {
+                score += 50; // Punktestand erhöhen
+                console.log(`Score: ${score}`); // Punktestand in der Konsole ausgeben
+                currencies.splice(i, 1); // Objekt entfernen
+                break; // Nur ein Objekt pro Klick entfernen
+            }
+        }
+    });
+}
+
+function update() {
+    currencies.forEach((currency) => {
+        currency.fall();
+    });
+}
+
+function draw() {
+    papier.clearRect(0, 0, flipchart.width, flipchart.height);
+    currencies.forEach((currency) => {
+        if (currency.width && currency.height) {
+            papier.drawImage(currency.bild, currency.x, currency.y, currency.width, currency.height);
+        }
+    });
+
+    drawID = window.requestAnimationFrame(draw);
 }
