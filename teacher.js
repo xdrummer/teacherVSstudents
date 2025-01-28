44330310e52d2f2186c741efc07b20f4654fb12a
Neruux <75936969+Neruux@users.noreply.github.com>
2025-01-28 09:14:50 +0100

    28_01_25

---
+++ teacher.js
@@ -0,0 +1,22 @@
+export class Teacher{
+    constructor(){
+        this.positionx;
+        this.positiony;
+        this.healthPoints = 100;
+        this.attackCooldown;
+        this.attackDamage;
+    }
+
+    setPositionx = function(){
+
+    }
+
+    setPositiony = function(){
+        
+    }
+
+
+
+
+
+}
