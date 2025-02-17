ace0cc28d82a7f351c4ad45b2e5e7726c36ac110
miklaren007 <133334282+miklaren007@users.noreply.github.com>
2025-02-17 18:42:59 +0100

    Add files via upload

--- Spielfeld.js
+++ Spielfeld.js
@@ -1,19 +1,38 @@
-export var flipchart = document.getElementById("Spielfeld");
-export var papier = flipchart.getContext("2d");
+var flipchart = document.getElementById("Spielfeld");
+var papier = flipchart.getContext("2d");
 
-export class Spielfeld{
+class Spielfeld{
     constructor(){
         this.r1 = [];
         this.r2 = [];
         this.r3 = [];
         this.r4 = [];
         this.r5 = [];
+    }
+    getX(f, s) {
+        let feld = this.getfelder(f);
+        return feld[s].x
+    }
 
+    getY(f, s) {
+        let feld = this.getfelder(f);
+        return  feld[s].y;
+    }
 
+    getfelder(f) {
+        switch (f) {
+            case 1: return this.r1;
+            case 2: return this.r2;
+            case 3: return this.r3;
+            case 4: return this.r4;
+            case 5: return this.r5;
+            break;
+        }
     }
 }
-export var neuesfeld = new Spielfeld()
-export class Feld{
+
+var neuesfeld = new Spielfeld()
+class Feld{
     constructor(x,y,w,h,s,l){
         this.x = x;
         this.y = y;
@@ -22,79 +41,80 @@ export class Feld{
         this.s = false;
         this.l = false
     }
+  
 }
-export function Spielfeldzeichnen(){
-    var verschub = 100;
-    let eins 
-    let zwei 
+function Spielfeldzeichnen(){
+var verschub = 100;
+let eins 
+let zwei 
 
-    for(let i=1; i < 6; i= i+1){
-        if(i % 2 == 0){
-            eins = "green"
-            zwei = "lightgreen"
-        }else{
-            eins = "lightgreen"
-            zwei = "green"  
+for(let i=1; i < 6; i= i+1){
+    if(i % 2 == 0){
+        eins = "green"
+        zwei = "lightgreen"
+    }else{
+        eins = "lightgreen"
+        zwei = "green"  
+    }
+    for(let j=1;j<10; j=j+1){
+        if(j % 2 == 0){
+            papier.fillStyle = eins
+            papier.fillRect((j* 100+verschub), (i*100 + verschub),100,100)
+            
+            switch(i){
+                case 1 : neuesfeld.r1.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
+                break;
+
+                case 2 : neuesfeld.r2.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
+                break;
+
+                case 3 : neuesfeld.r3.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
+                break;
+
+                case 4 : neuesfeld.r4.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
+                break;
+
+                case 5 : neuesfeld.r5.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
+                break;
+            }
+            //new Feld(j*100 + verschub, i*100 + verschub, 100, 100)}
+        
         }
-        for(let j=1;j<10; j=j+1){
-            if(j % 2 == 0){
-                papier.fillStyle = eins
-                papier.fillRect((j* 100+verschub), (i*100 + verschub),100,100)
+    
+        else if(j % 2 != 0){
+            papier.fillStyle = zwei
+            papier.fillRect((j* 100+verschub), (i*100 + verschub),100,100)
+            switch(i){
+                case 1 : neuesfeld.r1.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
                 
-                switch(i){
-                    case 1 : neuesfeld.r1.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
-                    break;
+                break;
 
-                    case 2 : neuesfeld.r2.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
-                    break;
+                case 2 : neuesfeld.r2.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
+                break;
 
-                    case 3 : neuesfeld.r3.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
-                    break;
+                case 3 : neuesfeld.r3.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
+                break;
 
-                    case 4 : neuesfeld.r4.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
-                    break;
+                case 4 : neuesfeld.r4.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
+                break;
 
-                    case 5 : neuesfeld.r5.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
-                    break;
-                }
-                //new Feld(j*100 + verschub, i*100 + verschub, 100, 100)}
-            
+                case 5 : neuesfeld.r5.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
+                break;
             }
+        }
         
-            else if(j % 2 != 0){
-                papier.fillStyle = zwei
-                papier.fillRect((j* 100+verschub), (i*100 + verschub),100,100)
-                switch(i){
-                    case 1 : neuesfeld.r1.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
-                    
-                    break;
-
-                    case 2 : neuesfeld.r2.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
-                    break;
-
-                    case 3 : neuesfeld.r3.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
-                    break;
-
-                    case 4 : neuesfeld.r4.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
-                    break;
-
-                    case 5 : neuesfeld.r5.push(new Feld(j*100 + verschub, i*100 + verschub, 100, 100))
-                    break;
-                }
-            }
-            
-        
+    
 
 
-        papier.fillStyle = "black"
-        papier.strokeRect(200,30,350,100)
-        papier.strokeRect(300,30,0.1,100)
-        papier.strokeRect(1000,1,100,50)
-        papier.font = "bold 35px Arial";  // Hier können Sie die Schriftart und -größe definieren
-        papier.fillText("Menü", 1004, 37)
+    papier.fillStyle = "black"
+    papier.strokeRect(200,30,350,100)
+    papier.strokeRect(300,30,0.1,100)
+    papier.strokeRect(1000,1,100,50)
+    papier.font = "bold 35px Arial";  // Hier können Sie die Schriftart und -größe definieren
+    papier.fillText("Menü", 1004, 37)
 
-    }
-    }
+}
+}
 /* for(let i=2; i < 6; i= i+2){
     for(let j=1;j<10; j=j+2){
     papier.fillStyle = "blue"
@@ -102,8 +122,9 @@ export function Spielfeldzeichnen(){
     }
 }
 // papier.fillStyle = "grey"
-// papier.fillRect(100,100,100,100)*/
+// papier.fillRect(100,100,100,100)
 
+*/
 
 }
-Spielfeldzeichnen();
+Spielfeldzeichnen(); 
